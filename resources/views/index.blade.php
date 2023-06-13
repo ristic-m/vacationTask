b0cafe93ddbb1357ce899ff1db3d29bd598bca59
ristic-m <risticm@icloud.com>
2023-06-13 17:23:44 +0200

    kjdsa

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,16 +1,30 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
 <body>
-    <form action="" method="post">
-        <input type="text" id="Staff">
+<form action="/show.blade.php" method="POST">
+    <label for="text">Name</label>
+    <input type="text">
 
-        <input type="date" name="vacay" id="vacay">
-        <button type="submit">yep</button>
-    </form>
+    <label for="text">Date of vacation</label>
+    <input type="text">
+
+    <label for="text">Availability</label>
+    <select name="availability" id="availability">
+        <option value=""></option>
+        <option value="remote">Remote</option>
+        <option value="unavailable">Unavailable</option>
+        <option value="uponRequest">Upon Request</option>
+    </select>
+
+    <button type="submit">Send</button>
+</form>
+
 </body>
 </html>
